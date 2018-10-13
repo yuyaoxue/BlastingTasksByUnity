aecbc286fed7225035b41f4ff36e945e4fa9f1b1
Yuyao <972934652@qq.com>
2018-10-13 13:46:37 +0800

    添加激活弹弓特效

---
+++ Assets/scripts/Slingshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject launchPoint;
+
+    private void Awake()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("OnMouseEnter");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("OnMouseExit");
+        launchPoint.SetActive(false);
+    }
+}
