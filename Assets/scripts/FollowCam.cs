c923f13fbe1fdc3f6b99a12ec6b0fbcf06fc3a42
Yuyao <972934652@qq.com>
2018-10-16 15:55:12 +0800

    添加爆破目标 - 城堡

--- Assets/scripts/FollowCam.cs
+++ Assets/scripts/FollowCam.cs
@@ -22,18 +22,36 @@ public class FollowCam : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (poi == null) return;
+        Vector3 destination;
 
-        Vector3 destination = poi.transform.position;
-        //限定 x 和 y 的最小值，避免当弹丸落地时摄像机拍摄到地面以下的画面
-        destination.x = Mathf.Max(minXY.x,destination.x);
-        destination.y = Mathf.Max(minXY.y, destination.y);
-        destination.z = _camz;
-        //在摄像机当前位置和目标位置之间增添插值
-        destination = Vector3.Lerp(transform.position, destination, easing);
-        //将跟随物体的坐标 赋值给 摄像机
-        transform.position = destination;
-        //设置 摄像机的 orthograohicSize, 使地面始终处于画面之中
-        _mainCamera.orthographicSize = destination.y + 10;
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = poi.transform.position;
+
+            if(poi.tag == "Projectile")
+            {
+                // 如果它处于 sleeping状态（即未移动）
+                if(poi.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    //返回默认视图
+                    poi = null;
+                    return;
+                }
+            }
+            //限定 x 和 y 的最小值，避免当弹丸落地时摄像机拍摄到地面以下的画面
+            destination.x = Mathf.Max(minXY.x, destination.x);
+            destination.y = Mathf.Max(minXY.y, destination.y);
+            destination.z = _camz;
+            //在摄像机当前位置和目标位置之间增添插值
+            destination = Vector3.Lerp(transform.position, destination, easing);
+            //将跟随物体的坐标 赋值给 摄像机
+            transform.position = destination;
+            //设置 摄像机的 orthograohicSize, 使地面始终处于画面之中
+            _mainCamera.orthographicSize = destination.y + 10;
+        }
     }
 }
